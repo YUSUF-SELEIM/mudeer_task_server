12df49f0d3fe84fa9ab960e19edf4bbf364941a9
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2025-03-16 23:22:57 +0200

    refactor express function to use native http request and response objects

--- src/functions/express.ts
+++ src/functions/express.ts
@@ -1,32 +1,31 @@
 import { Handler } from "@netlify/functions";
 import app from "../server";
+import http from "http";
+import url from "url";
 
 const handler: Handler = async (event, context) => {
 	const { path, httpMethod, headers, queryStringParameters, body } = event;
 
-	// Simulate an Express request
-	const request = {
-		path,
-		method: httpMethod,
-		headers,
-		query: queryStringParameters,
-		body: body ? JSON.parse(body) : null,
-	};
+	// Create a mock request object
+	const request = Object.create(http.IncomingMessage.prototype);
+	request.url = path;
+	request.method = httpMethod;
+	request.headers = headers;
+	request.query = queryStringParameters || {};
+	request.body = body ? JSON.parse(body) : null;
 
-	// Simulate an Express response
-	const response = {
-		statusCode: 200,
-		headers: {},
-		body: "",
-		setHeader(key: string, value: string) {
-			this.headers[key] = value;
-		},
-		send(data: string) {
-			this.body = data;
-		},
-		json(data: any) {
-			this.body = JSON.stringify(data);
-		},
+	// Create a mock response object
+	const response = Object.create(http.ServerResponse.prototype);
+	response.statusCode = 200;
+	response.headers = {};
+	response.setHeader = (key: string, value: string) => {
+		response.headers[key] = value;
+	};
+	response.send = (data: string) => {
+		response.body = data;
+	};
+	response.json = (data: any) => {
+		response.body = JSON.stringify(data);
 	};
 
 	// Handle the request with Express

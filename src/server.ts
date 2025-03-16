947fa79ae05ba5244b258b69873e539842af35c6
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2025-03-16 23:34:38 +0200

    add cors middleware and update package dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import data from "./data";
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 
 app.get("/notifications", (req: Request, res: Response) => {
 	res.json(data);
